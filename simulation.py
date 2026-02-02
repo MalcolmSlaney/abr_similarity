c9b9ac19496ce929474853ca009608de569747d9
Malcolm Slaney <malcolm@ieee.org>
2026-02-01 18:05:52 -0800

    Fixing stack label

--- simulation.py
+++ simulation.py
@@ -863,7 +863,7 @@ def plot_synthetic_stack_example(plot_dir: str = '.'):
                     xy = (num_points, -1.1),
                     arrowprops=dict(arrowstyle="->"))
 
-  plt.text(150, 1.0, 'Number of Trials (N)')
+  plt.text(180, 1.0, 'Number of \nTrials (N)')
   plt.text(75, -1.4, 'Time')
   plt.axis('off');
   plt.savefig(os.path.join(plot_dir, 'WaveformStack.png'), dpi=300)
